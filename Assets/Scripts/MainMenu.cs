062cdc9f6b38ce29122a3da33c1b151a082e1260
Alexander <kurbanov.alex@gmail.com>
2014-01-30 02:56:53 +0400

    Added 3 plane angle sliders

--- Assets/Scripts/MainMenu.cs
+++ Assets/Scripts/MainMenu.cs
@@ -3,25 +3,34 @@ using System.Collections;
 
 public class MainMenu : MonoBehaviour
 {
-	private float sliderValue1 = 0.0f;
-	private float sliderValue2 = 0.0f;
+	/*
+	 * 1 - Крен
+	 * 2 - Рыскание
+	 * 3 - Тангаж
+    */
+	private float[] sliderValues;
 
 	private float minValue = 0.0f;
-	private float maxValue = 10.0f;
+	private float maxValue = 90.0f;
+
+	void Start ()
+	{
+		sliderValues = new float[3];
+	}
 
 	void OnGUI ()
 	{
 		float left = 25.0f;
 		float top = 75.0f;
-		float width = 125.0f;
+		float width = 200.0f;
 		float height = 30.0f;
 
-		GUI.Box(new Rect(10, 10, left + width, 500), "Оси самолета");
+		GUI.Box(new Rect(10, 10, left + width, 320), "Углы самолета");
 
 		float textOffset = 25.0f;
 		float spaceBetweenSliders = 100.0f;
 
-		for (int i = 0; i < 2; i++) {
+		for (int i = 0; i < 3; i++) {
 			float position = top + i * spaceBetweenSliders;
 			Rect sliderRectangle = new Rect(left, position, width, height);
 
@@ -29,16 +38,17 @@ public class MainMenu : MonoBehaviour
 			float sliderValue = 0.0f;
 
 			if (i == 0) {
-				sliderValue1 = GUI.HorizontalSlider (sliderRectangle, sliderValue1, minValue, maxValue);
-				sliderText = "Первая ось:";
-				sliderValue = sliderValue1;
+				sliderText = "Крен:";
 			} else if (i == 1) {
-				sliderValue2 = GUI.HorizontalSlider (sliderRectangle, sliderValue2, minValue, maxValue);
-				sliderText = "Вторая ось:";
-				sliderValue = sliderValue2;
+				sliderText = "Рыскание:";
+			} else if (i == 2) {
+				sliderText = "Тангаж:";
 			}
+
+			sliderValues[i] = GUI.HorizontalSlider (sliderRectangle, sliderValues[i], minValue, maxValue);
+
 			GUI.Label (new Rect (left + textOffset, position - textOffset, width, height), sliderText);
-			GUI.Label (new Rect (left + width - textOffset, position - textOffset, width, height), sliderValue.ToString ());
+			GUI.Label (new Rect (left + width - (textOffset * 2), position - textOffset, width, height), sliderValues[i].ToString ());
 		}
 	}
 }
